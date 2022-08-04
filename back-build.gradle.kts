2b0487b707605bc6790973a7a5003092a61c06da
Jeong-Uk Park <dnr1105@gmail.com>
2022-08-04 22:03:54 +0900

    Clean build.gradle files

---
+++ back-build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.7.10"
+    val springBootVersion = "2.7.2"
+
+    id("org.springframework.boot") version springBootVersion apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+
+    kotlin("jvm") version kotlinVersion apply false
+    kotlin("kapt") version kotlinVersion apply false
+    kotlin("plugin.spring") version kotlinVersion apply false
+    kotlin("plugin.jpa") version kotlinVersion apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("kotlin")
+        plugin("java")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("kotlin-noarg")
+    }
+
+    group = "me.jeonguk"
+    version = "1.0-SNAPSHOT"
+
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "11"
+            }
+        }
+
+        withType<Test> {
+            useJUnitPlatform()
+        }
+    }
+}
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-
-rootProject.name = "spring-kotlin-playground"
-
