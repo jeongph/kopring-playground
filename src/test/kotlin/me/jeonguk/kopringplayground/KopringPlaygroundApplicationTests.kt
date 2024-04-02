ae5c070a4f15d57f3a46f5bf9dcc932cffa1b710
JeongUk Park <dnr1105@gmail.com>
2024-04-02 23:53:04 +0900

    init:

---
+++ src/test/kotlin/me/jeonguk/kopringplayground/KopringPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jeonguk.kopringplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
